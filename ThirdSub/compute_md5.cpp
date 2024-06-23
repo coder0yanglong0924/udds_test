f1f72149aa2a8737955d2abd9e6016ff167da20b
root <root@zyf>
2024-06-23 09:27:53 +0800

    第一题

---
+++ ThirdSub/compute_md5.cpp
@@ -0,0 +1,15 @@
+#include "compute_md5.hpp"
+
+void ComputeMD5::compute(const std::stringstream &buffer, unsigned char* md5_digest)
+{
+
+	MD5_Init(&ctx);
+
+	std::string str = buffer.str();
+
+	MD5_Update(&ctx, str.c_str(), str.size());
+
+	MD5_Final(md5_digest, &ctx);
+
+	return;
+}
